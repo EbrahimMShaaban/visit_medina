d04fb132adee2799582a4f45e6692cd5805da766
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2023-03-15 00:15:56 +0200

    Initial commit

---
+++ lib/shared/components/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+abstract class MediaQueryHelper {
+  static double sizeFromHeight(BuildContext context, double fraction,
+      {bool hasAppBar = true}) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    fraction = (hasAppBar ? (screenHeight - AppBar().preferredSize.
+    height -
+        MediaQuery.of(context).padding.top)
+        : screenHeight) /
+        fraction;
+    return fraction;
+  }
+
+  static double sizeFromWidth(BuildContext context, double fraction) {
+    return MediaQuery.of(context).size.width / fraction;
+  }
+}
+
+
+// appPadding(){
+//   return EdgeInsets.symmetric(horizontal: 30.h,vertical: 10.w);
+// }
---
+++ lib/shared/styles/styles.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  /// - weight: w200
+  /// - family: cairo
+
+
+
+
+  /// - weight: w300
+  /// - family: cairo
+
+
+  /// - weight: w400
+  /// - family: cairo
+
+
+  /// - weight: w500
+  /// - family: cairo
+
+
+  /// - weight: w600
+  /// - family: cairo
+  static TextStyle w600 = TextStyle(fontWeight: FontWeight.w600);
+
+  /// - weight: w700
+  /// - family: cairo
+
+
+  /// - weight: w800
+  /// - family: cairo
+  static TextStyle w800 = TextStyle(fontWeight: FontWeight.w800);
+
+  /// - weight: w900
+  /// - family: cairo
+
+}
